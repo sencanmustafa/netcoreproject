acf362a6e14d48bb5e195a4288165d78f3b52810
sencanmustafa <sencanmustafa54@gmail.com>
2022-08-11 16:43:01 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            int cat_id = 4;
+            Console.WriteLine("Get All");
+            foreach (var product in productManager.GetAll())
+            {
+
+                Console.WriteLine(product.ProductName);
+            }
+            Console.WriteLine($"Get All by Category id => {cat_id} ");
+            foreach (var product in productManager.GetAllByCategoryId(cat_id))
+            {
+                Console.WriteLine(product.ProductId);
+                Console.WriteLine(product.ProductName);
+            }
+              
+        }
+    }
+}
